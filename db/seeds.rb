5b193f74b6a53dba910b122aef157d7621015cc5
Soares Kim <kimuedasoares@gmail.com>
2023-01-12 21:26:53 +0900

    rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.destroy_all
+
+restaurant_one = Restaurant.create(name: "Lewagon1", address: "Hoxton Street", phone_number: "01234 578990", category: "chinese")
+restaurant_two = Restaurant.create(name: "Lewagon2", address: "Hoxton TwoStreet", phone_number: "01234 840200", category: "italian")
+restaurant_three = Restaurant.create(name: "Lewagon3", address: "Hoxton ThreeStreet", phone_number: "01234 283648", category: "japanese")
+restaurant_four = Restaurant.create(name: "Lewagon4", address: "Hoxton FourStreet", phone_number: "01234 627394", category: "french")
+restaurant_five = Restaurant.create(name: "Lewagon5", address: "Hoxton FiveStreet", phone_number: "01234 987123", category: "belgian")
